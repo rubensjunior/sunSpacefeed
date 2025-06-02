b85df9358102f13f87478a4e72fcf39e026533d3
rbentes <rbntes@gmail.com>
2025-06-02 09:23:27 -0400

    Layout pre-definido para login, cadastro de conta e dashboard.

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,28 @@
+dmx.config({
+  "signup": {
+    "signup_step": [
+      {
+        "name": "value",
+        "type": "number"
+      }
+    ],
+    "plano_value": [
+      {
+        "name": "value",
+        "type": "number"
+      }
+    ],
+    "polos_value": [
+      {
+        "name": "value",
+        "type": "number"
+      }
+    ],
+    "show_form": [
+      {
+        "name": "value",
+        "type": "boolean"
+      }
+    ]
+  }
+});
